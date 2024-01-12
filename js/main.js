8e235c29aab34eda27c2c5f4b1178fd57adf5700
brunosantosdesign <62440870+brunosantosdesign@users.noreply.github.com>
2024-01-12 00:47:27 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Button Whatss
+const Whats = document.querySelector("#Whats")
+
+Whats.addEventListener("click", () => {
+  window.open(
+    "https://wa.me/551313996159796?text=Olá! Seja bem vindo a 3S Drink'S Saboó. Agradecemos pelo seu contato!",
+    "_blank"
+  )
+})
+
+// ANIMATE AOS
+AOS.init({
+  duration: 1000,
+  once: true,
+})
